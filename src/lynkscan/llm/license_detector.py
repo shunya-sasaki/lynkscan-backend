aad5c6693635fde6778cad654fa2a58708250ccd
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2025-09-08 23:17:29 +0900

    feat: add LicenseDetector class for license identification using LLM

---
+++ src/lynkscan/llm/license_detector.py
@@ -0,0 +1,74 @@
+"""License checker with LLM."""
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama import ChatOllama
+from pydantic import BaseModel
+from pydantic import Field
+
+
+class License(BaseModel):
+    """License model."""
+
+    full_name: str = Field(
+        description="Full name of the license,"
+        + " e.g., 'Apache License 2.0', 'MIT License'."
+    )
+
+    identifier: str = Field(
+        description="SPDX identifier of the license,"
+        + " e.g., 'Apache-2.0', 'MIT'."
+    )
+
+
+class LicenseDetector:
+    """Detect license using LLM."""
+
+    def __init__(
+        self,
+        model: str = "gemma3:270m",
+        base_url: str = "http://localhost:11434",
+    ):
+        """Initialize the LicenseDetector."""
+        self.model = model
+        self.base_url = base_url
+        self.llm = ChatOllama(model=self.model, base_url=self.base_url)
+
+    def run(self, license_file: str, max_length: int = 1000) -> License:
+        """Run the license detector."""
+        with open(license_file, "r", encoding="utf-8", errors="ignore") as fin:
+            text = fin.read()
+        text = text[:max_length]
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    """
+            You are an expert in open source software licensing.
+            Your task is to identify the primary OSS lisense
+            from the input text.
+
+            Instructions:
+            - Focus on identifying starndard OSS lisenses such as Apache-2.0,
+              MIT, GPL-3.0, BSD-3Clause, etc.
+            - If multiple lisenses are mentioned, return the primary license
+              (the one that governs most of the code).
+            - Ignore project-specific names or contributors
+              unless they define a custom license.
+            - Return only the license name and SPDX identifier.
+            - Do not include unrelated terms, contributors,
+              or file names in the output.
+
+            Output format:
+            {{
+                'full_name': 'Apache Lisense 2.0',
+                'identifier': 'Apache-2.0'
+            }}
+            """,
+                ),
+                ("human", "{text}"),
+            ]
+        )
+        chain = prompt | self.llm.with_structured_output(License)
+        license = chain.invoke({"text": text})
+        license = License.model_validate(license)
+        return license
