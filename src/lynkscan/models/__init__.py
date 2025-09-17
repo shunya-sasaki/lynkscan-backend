b5c137c9237d161fee48b673eca13a3239a60095
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2025-09-17 23:36:35 +0900

    feat: add AppConfig model for application configuration

--- src/lynkscan/models/__init__.py
+++ src/lynkscan/models/__init__.py
@@ -1,6 +1,7 @@
 """Data models for LynkScan."""
 
+from lynkscan.models.app_config import AppConfig
 from lynkscan.models.license_category import LicenseCategory
 from lynkscan.models.license_info import LicenseInfo
 
-__all__ = ["LicenseCategory", "LicenseInfo"]
+__all__ = ["LicenseCategory", "LicenseInfo", "AppConfig"]
